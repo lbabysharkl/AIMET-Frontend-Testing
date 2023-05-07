859571bd6b6387228e90910d6880c361f15468c2
First.H <first.tanapol@gmail.com>
2023-05-07 15:04:29 +0700

    fix(.next): update .next file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f82e3651fd675e79"; }
+/******/ 		__webpack_require__.h = function() { return "6164d82f923ef7d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
